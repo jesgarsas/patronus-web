e0c922a24e49b5c3ff441d25eb584915b3dd338d
jesgarsas <jesgarsas@gmai.com>
2022-02-06 22:30:42 +0100

    Form creator

--- src/app/feature/ejercicio/ejercicio-manage/ejercicio-manage-creation/ejercicio-manage-creation.component.ts
+++ src/app/feature/ejercicio/ejercicio-manage/ejercicio-manage-creation/ejercicio-manage-creation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio-manage-creation',
@@ -12,12 +12,41 @@ export class EjercicioManageCreationComponent implements OnInit {
 
   readonly nombreFormName: string = 'nombre';
   readonly patronFormName: string = 'patron';
-  readonly intentosFormName: string = 'intentos'
+  readonly intentosFormName: string = 'intentos';
+  readonly preguntasFormName: string = 'preguntas';
+  readonly opcionesFormName: string = 'opciones';
+  readonly textoFormName: string = 'texto';
+  readonly tipoFormName: string = 'tipo';
+  readonly correcta: string = 'correcta';
 
   constructor() { }
 
   ngOnInit(): void {
     this.form = new FormGroup({});
+    this.form.addControl(this.preguntasFormName, new FormArray([]));
+  }
+
+  addPregunta() {
+    if (this.form && this.form.value[this.preguntasFormName]) {
+      let preguntaForm = new FormGroup({});
+      preguntaForm.addControl(this.textoFormName, new FormControl(undefined));
+      preguntaForm.addControl(this.opcionesFormName, new FormArray([
+        this.createOptionForm(),
+        this.createOptionForm(),
+      ]));
+      this.form.value[this.preguntasFormName].push(preguntaForm);
+    }
+  }
+
+  addOptionForm(form: FormArray) {
+    form.push(this.createOptionForm());
+  }
+
+  private createOptionForm() {
+    let form = new FormGroup({});
+    form.addControl(this.textoFormName, new FormControl(undefined));
+    form.addControl(this.correcta, new FormControl(undefined));
+    return form;
   }
 
 }
